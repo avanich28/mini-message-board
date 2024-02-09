b66535052fcd4505892915dd04e5a8ae2d12bcff
avanich28 <arpawan.v@gmail.com>
2024-02-09 15:59:33 +0700

    solve error (21)

--- client/src/services/apiMessage.js
+++ client/src/services/apiMessage.js
@@ -3,8 +3,10 @@ import { API_URL } from "../utils/constants";
 export async function getAllMessages() {
   const res = await fetch(`${API_URL}`, { mode: "no-cors" });
   if (!res.ok) throw new Error("Failed getting messages");
+  console.log(res);
 
   const data = await res.json();
+  console.log(data);
 
   return data.data.allMessages;
 }
