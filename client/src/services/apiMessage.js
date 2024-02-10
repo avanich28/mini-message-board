04a3942e22a00994f5fc1170e83b0af4f0f6a5b1
avanich28 <arpawan.v@gmail.com>
2024-02-10 22:06:15 +0700

    solve error (47)

--- client/src/services/apiMessage.js
+++ client/src/services/apiMessage.js
@@ -15,17 +15,17 @@ export async function getAllMessages() {
 
 export async function createMessage(newMessage) {
   try {
-    const res = await fetch(API_URL, {
+    await fetch(API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(newMessage),
     });
-    if (!res.ok) throw new Error("Failed creating your message");
+    // if (!res.ok) throw new Error("Failed creating your message");
 
-    const data = await res.json();
-    return data;
+    // const data = await res.json();
+    // return data;
   } catch (err) {
     console.error(err);
   }
