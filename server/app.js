4b73d996cc7cfb65eae645a05e3eda34c09bceaa
avanich28 <arpawan.v@gmail.com>
2024-02-10 17:45:50 +0700

    solve error (38)

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "development") {
 // };
 
 // app.use(cors(corsOpts));
-app.use(cors({ origin: "https://mini-message-board-virid.vercel.app" }));
+app.use(cors({ origin: "https://mini-message-board-virid.vercel.app/" }));
 app.options("/api/v1/message-board", cors());
 app.use(express.json({ limit: "10kb" }));
 app.use(xss());
