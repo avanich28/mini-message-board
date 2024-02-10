35e7e7dde24213e176223a37f7360b73827fe0e6
avanich28 <arpawan.v@gmail.com>
2024-02-10 20:29:14 +0700

    solve error (41)

--- server/app.js
+++ server/app.js
@@ -21,14 +21,15 @@ if (process.env.NODE_ENV === "development") {
 // };
 
 // app.use(cors(corsOpts));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 // app.use(cors({ origin: "https://mini-message-board-virid.vercel.app/" }));
 // app.options("/api/v1/message-board", cors());
+app.use(cors({ origin: true }));
 app.use(express.json({ limit: "10kb" }));
 app.use(xss());
 
