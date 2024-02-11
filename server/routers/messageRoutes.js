23a0be20829b7572c7e887ad07b4432634d4486c
avanich28 <arpawan.v@gmail.com>
2024-02-11 16:24:29 +0700

    solve error (49)

--- server/routers/messageRoutes.js
+++ server/routers/messageRoutes.js
@@ -3,9 +3,7 @@ const messageController = require("../controllers/messageController");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(messageController.getAllMessages)
-  .post(messageController.createMessage);
+router.get("/", messageController.getAllMessages);
+router.post("/", messageController.createMessage);
 
 module.exports = router;
